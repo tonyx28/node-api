b35ade464c5082efdd02bf66fac84dfcd3a0fc0a
Tony Xie <tony_xie@outlook.com>
2017-06-05 05:58:16 -0700

    updated mongodb to mlab test setup

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var app        = express();
 var bodyParser = require('body-parser');
 
 var mongoose   = require('mongoose');
-mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o')
+mongoose.connect('mongodb://noding:nodetest1@ds163721.mlab.com:63721/node-testing');
 // configure app to use bodyParser()
 // used to get data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;    // set port to 8080
 
 // ROUTES FOR OUR API
 // =====================================================================
@@ -29,6 +29,28 @@ router.use(function(req, res, next){
 router.get('/', function(req, res) {
   res.json({ message: 'hooray! welcome to our api!' });
 });
+
+// more routes for our API will happen here
+
+// on routes that end in /bears
+// ---------------------------------------------------------------------
+router.route('/bears')
+
+  // create a bear (accessed at POST http://localhost:8080/api/bears)
+  .post(function(req, res) {
+
+    var bear = new Bear();      // create a new instance of the Bear model
+    bear.name = req.body.name;  // set the bears name (comes from the request)
+
+    // save the bear and check for errors
+    bear.save(function(err) {
+      if (err)
+        res.send(err);
+
+      res.json({ message: 'Bear created!' });
+    });
+
+  });
 
 // REGISTER OUR ROUTES -------------------------------------------------
 // all routes will be prefixed by /api
