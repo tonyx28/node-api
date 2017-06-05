11c5d5fc04b092729224148a8482ca69149af6c4
Tony Xie <tony_xie@outlook.com>
2017-06-05 03:56:47 -0700

    connected mongodb

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express    = require('express');          // call express
 var app        = express();
 var bodyParser = require('body-parser');
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o')
 // configure app to use bodyParser()
 // used to get data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
